fea039d1be13e125b3dd8e079fe84db59d20a47c
Ahmed Alsudairi <a7madalsudairi@gmail.com>
2021-08-06 19:33:36 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by ahmed on 06/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
