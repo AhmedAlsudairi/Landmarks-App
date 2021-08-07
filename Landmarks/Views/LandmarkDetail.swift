7c3e39896212c1d946dbc08f469bf3bc1e9f5ce1
Ahmed Alsudairi <a7madalsudairi@gmail.com>
2021-08-07 19:20:40 +0300

    first commit

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by ahmed on 07/08/2021.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
