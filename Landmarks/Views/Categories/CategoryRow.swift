fb5646646de08d994cd374f9c4b9b91a969ad6f7
Ahmed Alsudairi <a7madalsudairi@gmail.com>
2021-08-08 16:35:48 +0300

    creating CategoryRow view

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,38 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by ahmed on 08/08/2021.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    var body: some View {
+        
+        VStack {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack{
+                    ForEach(items){
+                        item in
+                        Text(item.name)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    static var previews: some View {
+        CategoryRow(categoryName: landmarks[0].category.rawValue, items: Array(landmarks.prefix(4)))
+    }
+}
