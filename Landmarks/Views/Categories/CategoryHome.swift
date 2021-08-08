1b0ba1a5e792ac3b981d1abc9121180c0fdc71a3
Ahmed Alsudairi <a7madalsudairi@gmail.com>
2021-08-08 15:01:21 +0300

    creating CategoryHome View

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by ahmed on 08/08/2021.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView{
+            
+            List{
+                ForEach(modelData.categories.keys.sorted(), id: \.self){
+                    category in
+                    Text(category)
+                }
+            }
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
